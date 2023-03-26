7ac36ee06f3e2abdea53461b06c385c0924647e1
Nelson Pérez <bilthon@gmail.com>
2023-03-25 20:40:53 -0500

    feature: completed OrderStatus & Order interfaces

--- store/orders.ts
+++ store/orders.ts
@@ -4,14 +4,30 @@ export enum OrderType {
 }
 
 export enum OrderStatus {
+  ACTIVE = 'Active',
+  CANCELED = 'Canceled',
+  CANCELED_BY_ADMIN = 'CanceledByAdmin',
+  COMPLETED_BY_ADMIN = 'CompletedByAdmin',
+  DISPUTE = 'Dispute',
+  EXPIRED = 'Expired',
+  FIAT_SENT = 'FiatSent',
+  SETTLE_HODL_INVOICE = 'SettledHoldInvoice',
   PENDING = 'Pending',
-  // TODO: Get all possible order states
+  SUCCESS = 'Success',
+  WAITING_BUYER_INVOICE = 'WaitingBuyerInvoice',
+  WAITING_PAYMENT = 'WaitingPayment'
 }
 
 export interface Order {
   id: string,
   kind: OrderType,
-  status: OrderStatus
+  status: OrderStatus,
+  amount: number,
+  fiat_code: string,
+  fiat_amount: number,
+  payment_method: string,
+  prime: number,
+  created_at: number,
 }
 
 export const state = () => ({
