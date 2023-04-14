2e20b33d2246841ca447c02dceb3317309d083a3
Nelson <bilthon@gmail.com>
2023-04-14 01:18:42 -0500

    feature: adds some graphic resources

---
+++ components/NoOrders.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/no-orders.webp"
+        alt="No orders"
+        height="250"
+      >
+      <p class="text-h6 text--disabled font-weight-black">Sorry, I ate all the orders!</p>
+    </v-col>
+  </v-row>
+</template>
--- components/OrderList.vue
+++ components/OrderList.vue
@@ -1,35 +1,36 @@
 <template>
   <v-card class="mx-auto">
-    <v-list>
-    <v-list-item v-for="order in getPendingOrders"
-      :key="order.id"
-      three-line
-      link
-    >
-      <v-list-item-content>
-        <v-list-item-title class="d-flex justify-space-between">
-          {{ order.fiat_amount }} {{ order.fiat_code.toUpperCase() }}
-          <v-chip
-            style="cursor: pointer"
-            outlined
-            :color="order.kind === 'Sell' ? 'red' : 'green'"
-            small
-          >
-            {{ order.kind.toUpperCase() }}
-          </v-chip>
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          {{ summary(order) }}
-        </v-list-item-subtitle>
-        <v-list-item-subtitle>
-          <div class="d-flex justify-space-between">
-            {{ order.payment_method }}
-            <take-sell-order-dialog :order="order"/>
-          </div>
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+    <v-list v-if="getPendingOrders.length > 0">
+      <v-list-item v-for="order in getPendingOrders"
+        :key="order.id"
+        three-line
+        link
+      >
+        <v-list-item-content>
+          <v-list-item-title class="d-flex justify-space-between">
+            {{ order.fiat_amount }} {{ order.fiat_code.toUpperCase() }}
+            <v-chip
+              style="cursor: pointer"
+              outlined
+              :color="order.kind === 'Sell' ? 'red' : 'green'"
+              small
+            >
+              {{ order.kind.toUpperCase() }}
+            </v-chip>
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ summary(order) }}
+          </v-list-item-subtitle>
+          <v-list-item-subtitle>
+            <div class="d-flex justify-space-between">
+              {{ order.payment_method }}
+              <take-sell-order-dialog :order="order"/>
+            </div>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
+    <no-orders v-else/>
   </v-card>
 </template>
 
