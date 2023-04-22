281f1eed6e03a47265cc47779aa90e89ab5a6c08
Emiliano Díaz <ediaz@litebox.ai>
2023-04-21 21:39:46 -0300

    wip: eslint scaffolding

--- apps/mostro-cross-env-client/vite.config.ts
+++ apps/mostro-cross-env-client/vite.config.ts
@@ -1,26 +1,19 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-import vue from '@vitejs/plugin-vue'
-import vuetify  from 'vite-plugin-vuetify'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true }), ],
+  plugins: [vue(), vuetify({ autoImport: true }), eslint()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
     port: 3000,
   },
-})
+});
---
+++ configs/eslint-config-custom/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  plugins: ["@typescript-eslint", "prettier"],
+  parser: "@typescript-eslint/parser",
+};
