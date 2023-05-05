9e6a664858fc7b11865e266b121bd2973d4f1995
Nelson <bilthon@gmail.com>
2023-05-04 23:58:49 -0500

    refactor: re-wording ot the message in the PayInvoiceMessage component

--- components/PayInvoiceMessage.vue
+++ components/PayInvoiceMessage.vue
@@ -8,7 +8,7 @@
       {{ messageText }}
     </v-list-item-subtitle>
     <v-list-item-subtitle class="d-flex justify-space-between">
-      Please pay this invoice to start up your selling process, it will expire in 15 minutes
+      Please press the "SHOW INVOICE" button below to display the invoice you're expected to pay. It will expire in 15 minutes
     </v-list-item-subtitle>
   </v-list-item-content>
 </template>
