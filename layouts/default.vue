28358072f90b75f8d963deee53a7bde5e437fb7a
Nelson <bilthon@gmail.com>
2023-05-20 11:24:28 -0500

    feature: introduces basics of a notification menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,10 +29,48 @@
       app
       dark
       color="primary"
+      class="px-2"
     >
       <v-app-bar-nav-icon v-if="isMobile" @click.stop="drawer = !drawer" />
       <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer />
+      <v-menu
+        offset-y
+        content-class="pa-0"
+        v-model="menuOpen"
+        :close-on-click="false"
+        :close-on-content-click="false"
+        :close-delay="5000"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-badge dot color="accent">
+              <v-icon>mdi-bell-outline</v-icon>
+            </v-badge>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(notification, i) in notifications"
+            :key="i"
+            class="notification-item"
+          >
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ notification.title }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ notification.subtitle }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-btn text block @click.prevent="clearNotifications">
+              <v-icon>mdi-notification-clear-all</v-icon>
+            </v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -81,13 +119,44 @@ export default {
         }
       ],
       right: true,
-      title: 'Mostro'
+      title: 'Mostro',
+      menuOpen: false,
+      notifications: [
+        { title: 'Order taken', subtitle: 'Your sell order was taken, click here to proceed'},
+        { title: 'Order taken', subtitle: 'Your buy order was taken, click here to proceed'}
+      ]
     }
   },
   mounted() {
     if (!this.isMobile) {
       this.drawer = true
     }
+  },
+  methods: {
+    clearNotifications() {
+      this.notifications.forEach((_, index) => {
+        setTimeout(() => {
+          this.notifications.splice(index, 1);
+        }, index * 200);  // Delay each removal by 200ms
+      });
+    }
+  },
+  watch: {
+    notifications(newVal) {
+      if (newVal.length === 0) {
+        this.menuOpen = false;
+      }
+    }
   }
 }
 </script>
+<style scoped>
+.notification-item {
+  transition: all 0.5s ease;
+}
+
+.notification-item.leave-active {
+  opacity: 0;
+  transform: translateX(100%);
+}
+</style>
