38cfc0d05d1095905bca4d6c10653bd4e41f044a
Nelson <bilthon@gmail.com>
2023-09-02 12:31:25 -0500

    refactor: removes stepper logic

--- pages/my-trades/[id].vue
+++ pages/my-trades/[id].vue
@@ -11,28 +11,6 @@
       <message-list :order-id="$route.params.id"/>
     </div>
     <trade-actions></trade-actions>
-    <!-- <v-stepper alt-labels class="mt-5">
-      <v-stepper-header>
-        <v-stepper-step step="1" :complete="isWaitingPayment">
-          Waiting Payment
-        </v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="2" :complete="isActive">
-          Invoice Accepted
-        </v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="3" :complete="isFiatSent">
-          Fiat Sent
-        </v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="4" :complete="isFundsReleased">
-          Funds released
-        </v-stepper-step>
-      </v-stepper-header>
-    </v-stepper> -->
-    <!-- <div class="text-caption">
-      {{ getOrderStatus(route.params.id) }}
-    </div> -->
   </v-container>
 </template>
 <script lang="ts">
@@ -41,14 +19,6 @@ import { useRoute } from 'vue-router'
 import { useOrders } from '~/stores/orders'
 import { OrderStatus } from '~/stores/types'
 
-const steps = {
-  [`${OrderStatus.PENDING}`]: 0,
-  [`${OrderStatus.WAITING_PAYMENT}`]: 1,
-  [`${OrderStatus.ACTIVE}`]: 2,
-  [`${OrderStatus.FIAT_SENT}`]: 3,
-  [`${OrderStatus.SETTLE_HODL_INVOICE}`]: 4,
-  [`${OrderStatus.SUCCESS}`]: 4
-}
 export default defineComponent({
   layout: 'message-list',
   data() {
@@ -63,26 +33,6 @@ export default defineComponent({
     currentOrderStatus() : OrderStatus {
       // @ts-ignore
       return this.getOrderStatus(this.$route.params.id)
-    },
-    isOrderTaken() {
-      // @ts-ignore
-      return steps[this.currentOrderStatus] >= steps[OrderStatus.PENDING] ?? 0
-    },
-    isWaitingPayment() {
-      // @ts-ignore
-      return steps[this.currentOrderStatus] >= steps[OrderStatus.WAITING_PAYMENT] ?? 0
-    },
-    isActive() {
-      // @ts-ignore
-      return steps[this.currentOrderStatus] >= steps[OrderStatus.ACTIVE] ?? 0
-    },
-    isFiatSent() {
-      // @ts-ignore
-      return steps[this.currentOrderStatus] >= steps[OrderStatus.FIAT_SENT] ?? 0
-    },
-    isFundsReleased() {
-      // @ts-ignore
-      return steps[this.currentOrderStatus] >= steps[OrderStatus.SUCCESS] ?? 0
     },
     isCancelled() {
       return this.currentOrderStatus === OrderStatus.CANCELED
