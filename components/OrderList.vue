26d285317f66d03244512497b9f6aaea40b3af64
Nelson <bilthon@gmail.com>
2023-09-16 23:17:50 -0500

    refactor: adjusts order type tags

--- components/OrderList.vue
+++ components/OrderList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mx-auto">
-    <v-list v-if="getPendingOrders.length > 0">
+    <v-list v-if="getPendingOrders.length > 0" class="py-0">
       <div v-for="order in getPendingOrders" :key="order.id">
         <v-list-item
           three-line
@@ -9,13 +9,13 @@
           <v-list-item-title class="d-flex justify-space-between">
             {{ order.fiat_amount }} {{ order.fiat_code.toUpperCase() }} {{ getFlag(order.fiat_code) }}
             <v-chip
-              class="ma-2"
-              rounded
+              class="my-2 px-4 "
+              label
               size="small"
               style="cursor: pointer"
               :color="order.kind === 'Sell' ? 'red' : 'green'"
             >
-              {{ order.kind.toUpperCase() }}
+              {{ order.kind.toUpperCase() === 'BUY' ? 'BUY ORDER' : 'SELL ORDER' }}
             </v-chip>
           </v-list-item-title>
           <v-list-item-subtitle>
@@ -24,8 +24,8 @@
           <v-list-item-subtitle>
             <div class="d-flex justify-space-between">
               Payment via: {{ order.payment_method }} - id: {{ order.id }}
-              <take-sell-order-dialog v-if="showTakeSell(order)" :order="order"/>
-              <take-buy-order-dialog v-if="showTakeBuy(order)" :order="order"/>
+              <take-sell-order-dialog v-if="showTakeSell(order) && !order.is_mine" :order="order"/>
+              <take-buy-order-dialog v-if="showTakeBuy(order) && !order.is_mine" :order="order"/>
             </div>
           </v-list-item-subtitle>
         </v-list-item>
