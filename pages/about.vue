8c5a4d0d119ca4a88dcc986b636d030e11593dd0
Nelson <bilthon@gmail.com>
2024-07-03 00:49:15 -0500

    feat: expanded the 'about' page and added a link to the github repo

--- pages/about.vue
+++ pages/about.vue
@@ -5,9 +5,9 @@
         src="/mostro-logo.png"
         alt="Vuetify.js"
         class="mb-5"
-        height="200"
+        height="150"
       >
-      <blockquote class="blockquote">
+      <blockquote class="blockquote text-medium-emphasis">
         &#8220;It might make sense just to get some in case it catches on.&#8221;
         <footer>
           <small>
@@ -17,6 +17,26 @@
       </blockquote>
     </v-col>
   </v-row>
+  <v-row class="mt-12">
+    <v-col>
+      <p class="text-center text-body-1" style="max-width: 40em; margin: auto;">
+        Mostro Web is an open source web client that facilitates peer-to-peer bitcoin trading over the lightning network ⚡ using nostr.
+      </p>
+      <br>
+      <p class="text-center text-body-1">Contributions are always welcome!</p>
+      <div class="text-center mt-4">
+        <v-btn
+          href="https://github.com/MostroP2P/mostro-web"
+          target="_blank"
+          rel="noopener"
+          icon
+          color="white"
+        >
+          <v-icon size="x-large">mdi-github</v-icon>
+        </v-btn>  
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -24,3 +44,9 @@ export default {
   name: 'InspirePage'
 }
 </script>
+
+<style scoped>
+.v-footer {
+  background-color: transparent !important;
+}
+</style>
